d337e7d7d762d36bf65625c08f899fe5bf04b414
Andy <andrewjamesevans@gmail.com>
2024-06-16 16:13:04 +0100

    Changes

--- commute_route.py
+++ commute_route.py
@@ -3,32 +3,36 @@ import json
 from datetime import datetime, timezone, timedelta
 import secrets
 import screen_writer
+import time 
 
 at_time = datetime.now(timezone.utc) + timedelta(0, 0, 0, 0, 2)
 
-request = {
-  "origin":{
-    "address": secrets.route_from
-  },
-  "destination":{
-    "address": secrets.route_to
-  },
-  "travelMode": "DRIVE",
-  "routingPreference": "TRAFFIC_AWARE",
-  "departureTime": at_time.isoformat(),
-  "computeAlternativeRoutes": "true",
-  "routeModifiers": {
-    "avoidTolls": "false",
-    "avoidHighways": "false",
-    "avoidFerries": "false"
-  },
-  "languageCode": "en-GB",
-  "units": "IMPERIAL"
-}
+def get_quickest_route():
+  request = {
+    "origin":{
+      "address": secrets.route_from
+    },
+    "destination":{
+      "address": secrets.route_to
+    },
+    "travelMode": "DRIVE",
+    "routingPreference": "TRAFFIC_AWARE",
+    "departureTime": at_time.isoformat(),
+    "computeAlternativeRoutes": "true",
+    "routeModifiers": {
+      "avoidTolls": "false",
+      "avoidHighways": "false",
+      "avoidFerries": "false"
+    },
+    "languageCode": "en-GB",
+    "units": "IMPERIAL"
+  }
 
-response = requests.post('https://routes.googleapis.com/directions/v2:computeRoutes', json.dumps(request), headers= { 'X-Goog-Api-Key': secrets.api_key, 'X-Goog-FieldMask': 'routes.description,routes.duration', 'Content-Type': 'application/json'})
+  response = requests.post('https://routes.googleapis.com/directions/v2:computeRoutes', json.dumps(request), headers= { 'X-Goog-Api-Key': secrets.api_key, 'X-Goog-FieldMask': 'routes.description,routes.duration', 'Content-Type': 'application/json'})
 
-routes = response.json()
+  routes = response.json()
+
+  return min(routes['routes'], key=lambda x: x['duration'])
 
 def getMinutesFromSecondsSetting(str):
     seconds = int(str.strip('s')) / 60
@@ -41,11 +45,14 @@ def transformDescription(description):
         case "Abbey Ln/B6068": return "Ecclesall Rd"
         case _: return description
 
-shortest_route = min(routes['routes'], key=lambda x: x['duration'])
+while(True):
+  print("Getting quickest route...")
+  shortest_route = get_quickest_route()
 
+  route = transformDescription(shortest_route['description'])
+  duration_mins = getMinutesFromSecondsSetting(shortest_route['duration'])
 
-route = transformDescription(shortest_route['description'])
-duration_mins = getMinutesFromSecondsSetting(shortest_route['duration'])
+  writer = screen_writer.ScreenWriter()
+  writer.show_route(route, duration_mins, at_time)
 
-writer = screen_writer.ScreenWriter()
-writer.show_route(route, duration_mins, at_time)
+  time.sleep(60)
